77b3a958d60de2a9b728a16ffdd5264df6fb29f5
Thomas Pappas <thomas@backbone.gr>
2021-11-30 20:04:17 +0200

    3D Game: 10.3-10.5: Player camera mouse movement and nav mesh

---
+++ 3D Game/Assets/Game/Scripts/LookX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.localEulerAngles += new Vector3(0, mouseX, 0);
+    }
+}
--- 3D Game/Assets/Game/Scripts/Player.cs
+++ 3D Game/Assets/Game/Scripts/Player.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
     private CharacterController controller;
     private float speed = 3.5f;
     private float gravity = 9.81f;
+
+    [SerializeField]
+    private NavMeshAgent navMeshAgent;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,8 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         Vector3 velocity = direction * speed;
         velocity.y -= gravity;
+        velocity = transform.TransformDirection(velocity);
         controller.Move(velocity * Time.deltaTime);
+        transform.position = navMeshAgent.nextPosition;
     }
 }
